4e9c9f43f9d504a4db0db6aa8f2a734fcf5be3c8
andrew-oxenburgh <andrew.oxenburgh@gmail.com>
2023-12-04 01:06:36 +0000

    minor file change

--- src/__tests__/demo.test.tsx
+++ src/__tests__/demo.test.tsx
@@ -1,12 +1,12 @@
-import words from '../wordle/wordle-words'
+import words from '../wordleOne/wordle-words'
 
 describe('demo tests', () => {
-  test('expected to fail', () => {
-    expect(words.length).toBe(2315);
-  });
+   test('expected to fail', () => {
+      expect(words.length).toBe(2315);
+   });
 
-  test('expected to pass', () => {
-    expect(1 + 1).toBe(2);
-  });
+   test('expected to pass', () => {
+      expect(1 + 1).toBe(2);
+   });
 });
-export {}
+export { }
