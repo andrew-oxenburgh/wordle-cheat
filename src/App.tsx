efef9b747c1a06a84ddf05936fe8619683811e17
andrew-oxenburgh <andrew.oxenburgh@gmail.com>
2023-12-04 00:02:45 +0000

    Adding router and home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,31 @@
-
-import WordleCheat from './wordle/WordleCheat'
+import WordleCheat from './wordle/WordleCheat';
+import Home from './home/Home';
+import { Navbar, Container, Nav } from 'react-bootstrap';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 export default function App() {
    return (
       <>
+         <Navbar bg="dark" variant="dark" expand="lg">
+            <Container>
+               <Navbar.Brand href="/">Wordle Helper</Navbar.Brand>
+               <Navbar.Toggle aria-controls="basic-navbar-nav" />
+               <Navbar.Collapse id="basic-navbar-nav">
+                  <Nav className="me-auto">
+                     <Nav.Link href="/">Home</Nav.Link>
+                     <Nav.Link href="/cheat">Wordle Cheat</Nav.Link>
+                  </Nav>
+               </Navbar.Collapse>
+            </Container>
+         </Navbar>
          <main>
-            <WordleCheat />
+            <Router>
+               <Routes>
+                  <Route path="/" element={<Home />} />
+                  <Route path="/cheat" element={<WordleCheat />} />
+               </Routes>
+            </Router>
          </main>
       </>
-   )
+   );
 }
---
+++ src/home/Home.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Container, Button } from 'react-bootstrap';
+
+function Home() {
+   return (
+      <Container>
+         <h1>Welcome to Wordle Helper!</h1>
+         <p>This is a simple tool to assist you in solving Wordle puzzles.</p>
+         <p>
+            <Button variant="primary" href="/cheat">Start Cheating</Button>
+         </p>
+      </Container>
+   );
+};
+
+export default Home;
