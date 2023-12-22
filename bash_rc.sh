63d7d5bccb5926509ad8828db89da59e8f3d6ce7
Andrew Oxenburgh <andrew,oxenburgh@gmail.com>
2023-12-22 03:54:47 +0000

    some refactoring

---
+++ bash_rc.sh
@@ -0,0 +1,8 @@
+#/!/bin/bash 
+
+echo "inited"
+
+git config --global user.email andrew,oxenburgh@gmail.com
+git config --global user.name "Andrew Oxenburgh"
+alias gst='git status'
+alias gac='git commit -am '
