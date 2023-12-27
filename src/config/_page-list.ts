10e5dafb27bbad7357f0baf11d620ff319f1ddac
Andrew Oxenburgh <andrew.oxenburgh@gmail.com>
2023-12-28 12:40:27 +1300

    use eslint

--- src/config/_page-list.ts
+++ src/config/_page-list.ts
@@ -2,115 +2,115 @@ import * as R from 'ramda'
 import { startCase } from 'lodash'
 
 export type PageNameType = 'home' | 'welcome' | 'emoji-search'
-   | 'print-page' | 'wordle-one' | 'wordle-two' | 'text-helper'
-   | 'netflix-genre' | 'sumner-tides' | 'draggey-gamey' | 'password-generator'
+| 'print-page' | 'wordle-one' | 'wordle-two' | 'text-helper'
+| 'netflix-genre' | 'sumner-tides' | 'draggey-gamey' | 'password-generator'
 
 export type PageInfoType = {
-   title: string;
-   link: string;
-   name: PageNameType;
-   bg: string,
-   nextPage: string;
-   prevPage: string;
-   underConstruction: boolean;
-   shortDesc: string;
-   longDesc: string[];
+    title: string
+    link: string
+    name: PageNameType
+    bg: string
+    nextPage: string
+    prevPage: string
+    underConstruction: boolean
+    shortDesc: string
+    longDesc: string[]
 }
 
 export const _initPageList: Partial<PageInfoType>[] = [
-   {
-      name: 'welcome',
-      title: 'Andrew\'s Sketch Pad',
-      longDesc: [
-         'This is a showcase of some doodles by me.',
-         'Seriously, just sketches, you know, for fun.',
-         'I\'ve written heaps of these things, and it\'s nice to get them all under the one roof',
-      ],
-      link: '#/welcome',
-   },
-   {
-      name: 'emoji-search',
-      longDesc: [
-         'Very brutal look up.',
-         'I\'m using an API, but it\'s just a single call. Nothing complicated',
-      ],
-      link: '#/emoji-search',
-   },
-   {
-      name: 'print-page',
-      longDesc: [
-         'Print Page',
-         'Sometimes you just want to print off some text, a list or something. ',
-         'With this Doodle you can paste the text in and do some formatting using Markdown, and then print it out. ',
-      ],
-      link: '#/print-page',
-   },
-   {
-      name: 'wordle-one',
-      longDesc: [
-         'My first attempt at a Wordle Cheat.',
-         'It works, but it\'s a bit dodgy',
-      ],
-   },
-   {
-      name: 'wordle-two',
-      longDesc: [
-         'My second attempt at a Wordle Cheat.',
-         'Still very much a work in progress',
-      ],
-      underConstruction: true,
-   },
-   {
-      name: 'text-helper',
-      longDesc: [
-         'Paste in some text and it\'ll make suggestions about what to do with it.',
-      ],
-   },
-   {
-      name: 'netflix-genre',
-      longDesc: [
-         'Netflix provides a bunch of codes that aren\'t in their app.',
-         'Here they all, with links to Netflix.',
-      ],
-   },
-   {
-      name: 'sumner-tides',
-      longDesc: [
-         'Still a WIP',
-      ],
-      underConstruction: true,
-   },
-   {
-      name: 'draggey-gamey',
-      longDesc: [
-         'Play with this. It doesn\'t quite work on mobile',
-         'click on squares to change their colours. Drag a ' +
+    {
+        name: 'welcome',
+        title: 'Andrew\'s Sketch Pad',
+        longDesc: [
+            'This is a showcase of some doodles by me.',
+            'Seriously, just sketches, you know, for fun.',
+            'I\'ve written heaps of these things, and it\'s nice to get them all under the one roof',
+        ],
+        link: '#/welcome',
+    },
+    {
+        name: 'emoji-search',
+        longDesc: [
+            'Very brutal look up.',
+            'I\'m using an API, but it\'s just a single call. Nothing complicated',
+        ],
+        link: '#/emoji-search',
+    },
+    {
+        name: 'print-page',
+        longDesc: [
+            'Print Page',
+            'Sometimes you just want to print off some text, a list or something. ',
+            'With this Doodle you can paste the text in and do some formatting using Markdown, and then print it out. ',
+        ],
+        link: '#/print-page',
+    },
+    {
+        name: 'wordle-one',
+        longDesc: [
+            'My first attempt at a Wordle Cheat.',
+            'It works, but it\'s a bit dodgy',
+        ],
+    },
+    {
+        name: 'wordle-two',
+        longDesc: [
+            'My second attempt at a Wordle Cheat.',
+            'Still very much a work in progress',
+        ],
+        underConstruction: true,
+    },
+    {
+        name: 'text-helper',
+        longDesc: [
+            'Paste in some text and it\'ll make suggestions about what to do with it.',
+        ],
+    },
+    {
+        name: 'netflix-genre',
+        longDesc: [
+            'Netflix provides a bunch of codes that aren\'t in their app.',
+            'Here they all, with links to Netflix.',
+        ],
+    },
+    {
+        name: 'sumner-tides',
+        longDesc: [
+            'Still a WIP',
+        ],
+        underConstruction: true,
+    },
+    {
+        name: 'draggey-gamey',
+        longDesc: [
+            'Play with this. It doesn\'t quite work on mobile',
+            'click on squares to change their colours. Drag a ' +
          'square to change the squares over which you have dragged that colour',
-      ],
-   },
-   {
-      name: 'password-generator',
-      longDesc: [
-         'Generate a "good" password.',
-      ],
-   },
-   // {
-   //    name: 'blogs',
-   //    link: '/markdown/examplePage',
-   // },
+        ],
+    },
+    {
+        name: 'password-generator',
+        longDesc: [
+            'Generate a "good" password.',
+        ],
+    },
+    // {
+    //    name: 'blogs',
+    //    link: '/markdown/examplePage',
+    // },
 ]
 
 const fulfilPageDefaults = (page: Partial<PageInfoType>) => {
-   const title = startCase(page.name)
-   return {
-      title,
-      longDesc: [
-         title,
-      ],
-      shortDesc: title,
-      link: '/#/' + page.name,
-      underConstruction: false,
-      ...page,
-   }
+    const title = startCase(page.name)
+    return {
+        title,
+        longDesc: [
+            title,
+        ],
+        shortDesc: title,
+        link: '/#/' + page.name,
+        underConstruction: false,
+        ...page,
+    }
 }
 export const pageConfig: PageInfoType[] = R.map(fulfilPageDefaults, _initPageList)
--- src/pages/wordle-one/wordle-utils.ts
+++ src/pages/wordle-one/wordle-utils.ts
@@ -1,130 +1,130 @@
 import * as R from 'ramda'
 
 const getWords = async (): Promise<string[]> => {
-   const ret = await import('../../config/five-letter-words')
-   return ret.default
+    const ret = await import('../../config/five-letter-words')
+    return ret.default
 }
 
 const LOWERCASE_ALPHA = 'abcdefghijklmnopqrstuvwxyz'.split('')
 
 export const findWordsWithoutTheseLetters = async (form: WordleForm): Promise<string[]> => {
-   let ignoreLetters = form.unused
-   ignoreLetters = ignoreLetters + R.map((pos: any) => {
-      return pos.known + pos.unknown
-   })(form.position)
-   const letters = '[^' + R.uniq(ignoreLetters).join('') + ']'
-   const re = new RegExp(`^${R.repeat(letters, 5).join('')}$`)
-   const words = await getWords()
-   return R.reduce((acc: any, word: any) => {
-      if (re.test(word)) {
-         acc.push(word)
-      }
-      return acc
-   })([], words)
+    let ignoreLetters: string[] = form.unused
+    ignoreLetters = ignoreLetters + R.map((pos: any) => {
+        return pos.known + pos.unknown
+    })(form.position)
+    const letters = '[^' + R.uniq(ignoreLetters).join('') + ']'
+    const re = new RegExp(`^${R.repeat(letters, 5).join('')}$`)
+    const words = await getWords()
+    return R.reduce((acc: any, word: any) => {
+        if (re.test(word)) {
+            acc.push(word)
+        }
+        return acc
+    })([], words)
 }
 
 export const createRegEx = async (form: WordleForm): Promise<string[]> => {
-   const unused = (fm: any, off: any) => {
-      if (fm.position[off].known) {
-         return fm.position[off].known[0]
-      }
-      let ret = `[^${fm.unused}${fm.position[off].unknown}]`
-      if (ret === '[^]') {
-         ret = '.'
-      }
-      return ret
-   }
-   let allRightLetters = R.flatten(R.reduce((acc: any, pos: any) => {
-      acc.push(pos.known)
-      acc.push(pos.unknown)
-      return acc
-   }, [], form.position)).join('')
-   allRightLetters = allRightLetters.split('')
-   allRightLetters = R.uniq(allRightLetters).join('')
-   allRightLetters = R.sort((a: string, b: string) => a.localeCompare(b), allRightLetters)
-   allRightLetters = R.join('', allRightLetters)
+    const unused = (fm: any, off: any) => {
+        if (fm.position[off].known) {
+            return fm.position[off].known[0]
+        }
+        let ret = `[^${fm.unused}${fm.position[off].unknown}]`
+        if (ret === '[^]') {
+            ret = '.'
+        }
+        return ret
+    }
+    let allRightLetters = R.flatten(R.reduce((acc: any, pos: any) => {
+        acc.push(pos.known)
+        acc.push(pos.unknown)
+        return acc
+    }, [], form.position)).join('')
+    allRightLetters = allRightLetters.split('')
+    allRightLetters = R.uniq(allRightLetters).join('')
+    allRightLetters = R.sort((a: string, b: string) => a.localeCompare(b), allRightLetters)
+    allRightLetters = R.join('', allRightLetters)
 
-   const letters = [
-      unused(form, 0),
-      unused(form, 1),
-      unused(form, 2),
-      unused(form, 3),
-      unused(form, 4),
-   ]
-   const regexp = `^${letters.join('')}$`
+    const letters = [
+        unused(form, 0),
+        unused(form, 1),
+        unused(form, 2),
+        unused(form, 3),
+        unused(form, 4),
+    ]
+    const regexp = `^${letters.join('')}$`
 
-   const re = new RegExp(regexp)
-   const words = await getWords()
+    const re = new RegExp(regexp)
+    const words = await getWords()
 
-   return R.reduce((acc: string[], word: string) => {
-      const lettersInCommon = R.intersection(allRightLetters, word).join('')
-      if (re.test(word) && (lettersInCommon.length === allRightLetters.length)) {
-         acc.push(word)
-      }
-      return acc
-   })([], words)
+    return R.reduce((acc: string[], word: string) => {
+        const lettersInCommon = R.intersection(allRightLetters, word).join('')
+        if (re.test(word) && (lettersInCommon.length === allRightLetters.length)) {
+            acc.push(word)
+        }
+        return acc
+    })([], words)
 }
 
 export function sanitise(value: string): string {
-   let v: string[] = value.toLowerCase().split('')
-   v = R.uniq(v)
-   v = R.without([','], v)
-   v = R.intersection(v, LOWERCASE_ALPHA)
-   return v.join('')
+    let v: string[] = value.toLowerCase().split('')
+    v = R.uniq(v)
+    v = R.without([','], v)
+    v = R.intersection(v, LOWERCASE_ALPHA)
+    return v.join('')
 }
 
 export function retrieveJsonFromStorageWithDefault(key: string, defaultValue: WordleForm): WordleForm {
-   try {
-      const item: (string | null) = window.localStorage.getItem(key)
-      if (item) {
-         return JSON.parse(item)
-      } else {
-         return { ...defaultValue }
-      }
-   } catch (err) {
-      return { ...defaultValue }
-   }
+    try {
+        const item: (string | null) = window.localStorage.getItem(key)
+        if (item) {
+            return JSON.parse(item)
+        } else {
+            return { ...defaultValue }
+        }
+    } catch (err) {
+        return { ...defaultValue }
+    }
 }
 
 export function putJsonInStorage(key: string, value: WordleForm): void {
-   window.localStorage.setItem(key, JSON.stringify(value))
+    window.localStorage.setItem(key, JSON.stringify(value))
 }
 
 export const WORDLE_CHEAT_FORM = 'WORDLE_CHEAT_FORM'
 
 export interface PositionEntity {
-   known: string
-   unknown: string
+    known: string
+    unknown: string
 }
 
 export interface WordleForm {
-   unused: (string)[]
-   position: (PositionEntity)[];
+    unused: (string)[]
+    position: (PositionEntity)[]
 }
 
 
 export const FORM: WordleForm = {
-   unused: [],
-   position: [
-      {
-         known: '',
-         unknown: ''
-      },
-      {
-         known: '',
-         unknown: ''
-      },
-      {
-         known: '',
-         unknown: ''
-      },
-      {
-         known: '',
-         unknown: ''
-      },
-      {
-         known: '',
-         unknown: ''
-      },
-   ]
+    unused: [],
+    position: [
+        {
+            known: '',
+            unknown: '',
+        },
+        {
+            known: '',
+            unknown: '',
+        },
+        {
+            known: '',
+            unknown: '',
+        },
+        {
+            known: '',
+            unknown: '',
+        },
+        {
+            known: '',
+            unknown: '',
+        },
+    ],
 }
