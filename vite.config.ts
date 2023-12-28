c7b9cf5e07a20ed66a2fb26b66c11fbc1480b1bb
Andrew Oxenburgh <andrew.oxenburgh@gmail.com>
2023-12-28 18:06:07 +1300

    vite config now in .ts

---
+++ vite.config.ts
@@ -0,0 +1,39 @@
+/** @type {import('vite').UserConfig} */
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import express from './express-plugin'
+
+
+// https://vitejs.dev/config/
+const config = () => {
+   return {
+      plugins: [
+         react({
+            // so I can use preact-signals-react in my components
+            jsxRuntime: "classic",
+         }),
+         {
+            name: "markdown-loader",
+            transform(code, id) {
+               if (id.slice(-3) === ".md") {
+                  // For .md files, get the raw content
+                  return `export default ${JSON.stringify(code)};`;
+               }
+            }
+         },
+         express('src/express/index')
+      ],
+      server: {
+         host: '0.0.0.0',
+      },
+      css: {
+         preprocessorOptions: {
+            scss: {
+               // additionalData: `@import "./src/config/mvp-override.scss";`
+            }
+         }
+      }
+   }
+}
+export default defineConfig(config)
