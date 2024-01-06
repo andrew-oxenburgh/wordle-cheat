a0ef11a211cca72b1947b24f4d50962d14bd7ed2
Andrew Oxenburgh <andrew.oxenburgh@gmail.com>
2024-01-07 12:01:18 +1300

    lint

--- src/components/ButtonWithTooltip.tsx
+++ src/components/ButtonWithTooltip.tsx
@@ -8,6 +8,7 @@ type ButtonWithTooltipProps = {
     children?: string | JSX.Element | JSX.Element[]
     placement?: 'top' | 'bottom' | 'left' | 'right' | 'auto'
 } & ButtonProps
+
 export const ButtonWithTooltip: React.FC<ButtonWithTooltipProps> = (
     { placement = 'bottom', tipText, children, ...args }) => {
 
