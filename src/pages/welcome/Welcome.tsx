7b287159e0a550455e1251e92c41129970150020
Andrew Oxenburgh <andrew.oxenburgh@gmail.com>
2024-01-17 19:35:25 +1300

    Welcome page now has Welcome.tsx

---
+++ src/pages/welcome/Welcome.tsx
@@ -0,0 +1,38 @@
+import ReactMarkdown from 'react-markdown'
+import PageBody from '#/components/structural/PageBody'
+import Card from 'react-bootstrap/esm/Card'
+
+
+const Welcome = () => {
+    return (
+        <PageBody name="welcome">
+            <Card style={{ maxWidth: '30em', margin: 'auto' }}>
+                <Card.Header>
+                    Sketch Pad
+                </Card.Header>
+                <Card.Body>
+                    <ReactMarkdown children={`
+I've written a bunch of these over years, and thought it'd be nice to get them all in one place.
+
+I'm using Vite/React/Bootstrap with a Google Cloud Functions back end.
+
+Like:
+ - replit
+ - react in a proper app
+ - replit extensions
+ - dynamic imports
+ - get better at Ramda
+ - Vite
+ - Make a couple of these things more public
+ - code chunking by using dynamic imports
+ - trying out various techniques, like 
+         `} />
+                </Card.Body>
+            </Card >
+        </PageBody>
+    )
+}
+
+export default Welcome
+
+
--- src/pages/welcome/index.tsx
+++ src/pages/welcome/index.tsx
@@ -1,35 +1,3 @@
-import ReactMarkdown from 'react-markdown'
-import PageBody from '#/components/structural/PageBody'
-import Card from 'react-bootstrap/esm/Card'
-
-
-const Welcome = () => {
-    return (
-        <PageBody name="welcome">
-            <Card style={{ maxWidth: '30em', margin: 'auto' }}>
-                <Card.Header>
-                    A Sketchpad, so I can try out a few different things.
-                </Card.Header>
-                <Card.Body>
-                    <ReactMarkdown children={`
-
-Like:
- - replit
- - react in a proper app
- - replit extensions
- - dynamic imports
- - get better at Ramda
- - Vite
- - Make a couple of these things more public
- - code chunking by using dynamic imports
- - trying out various techniques, like 
-         `} />
-                </Card.Body>
-            </Card >
-        </PageBody>
-    )
-}
+import Welcome from './Welcome'
 
 export default Welcome
-
-
