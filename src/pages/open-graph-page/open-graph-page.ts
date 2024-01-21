947fd13148bf11d8ea3c2e60cecd5e793bb1dad0
Andrew Oxenburgh <andrew.oxenburgh@gmail.com>
2024-01-21 13:23:10 +1300

    fixed error handling in OG fetch

--- src/pages/open-graph-page/open-graph-page.ts
+++ src/pages/open-graph-page/open-graph-page.ts
@@ -3,18 +3,33 @@ import { getFunctions, httpsCallable } from 'firebase/functions'
 import { OgObject } from 'open-graph-scraper/dist/lib/types'
 
 import app from '#/config/firebase'
+import { loggerError } from '#/config/logger'
+import { isDevMode } from '#/config/isDevMode'
 
 export const getOgGraph = async (url: string): Promise<OgObject> => {
-    const functions = getFunctions(app)
-    const httpCaller = httpsCallable(functions, 'ogGraph', {
+    try {
+        const functions = getFunctions(app)
+        const httpCaller = httpsCallable(functions, 'ogGraph', {
 
-    })
+        })
 
-    const result = await httpCaller({ url }) as {
-        data: {
-            graph: OgObject
+        const result = await httpCaller({ url }) as {
+            data: {
+                graph: OgObject
+            }
+        }
+        const json = result?.data.graph || { success: false } as OgObject
+        return json
+    } catch (error) {
+        loggerError(error)
+        const msg = isDevMode() ?
+            'Error: in Dev mode. are you running the emulator?'
+            : 'Error: not in DEV mode. Are your Google Cloud Functions there?'
+
+        loggerError(msg)
+        return {
+            success: false,
+            error: msg,
         }
     }
-    const json = result?.data.graph || { success: false } as OgObject
-    return json
 }
