a7b12d69285eae9c14079b54aa07629cf17d11f5
andrew-oxenburgh <andrew.oxenburgh@gmail.com>
2024-03-09 07:22:39 +1300

    removed draggable list

--- src/pages/drag-n-drop/index.tsx
+++ src/pages/drag-n-drop/index.tsx
@@ -1,14 +1,13 @@
 // PAGE: drag-n-drop
 
 import PageBody from '#/components/structural/PageBody'
-import Example from './Example'
 
 const DragNDropWrapper: React.FC = () => {
     return (
         <div
         >
             <PageBody name="drag-n-drop">
-                <Example />
+                <h1>kjhkjhkjhkj</h1>
             </PageBody >
         </div>
     )
