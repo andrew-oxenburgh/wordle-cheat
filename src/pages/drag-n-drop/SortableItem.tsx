b57ddd73da6bde7a009c4df2bcde42ae80f080e9
andrew-oxenburgh <andrew.oxenburgh@gmail.com>
2024-03-10 11:59:05 +1300

    now use jss

--- src/pages/drag-n-drop/SortableItem.tsx
+++ src/pages/drag-n-drop/SortableItem.tsx
@@ -3,9 +3,34 @@ import { useSortable } from '@dnd-kit/sortable'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faEllipsisVertical as icon } from '@fortawesome/free-solid-svg-icons/faEllipsisVertical'
 import Webcam from 'react-webcam'
-import Button from 'react-bootstrap/esm/Button'
+import Button from 'react-bootstrap/Button'
 import { useCallback, useRef } from 'react'
+import { createUseStyles } from 'react-jss'
 
+const useStyles = createUseStyles({
+    card: {
+        border: '1px orange solid',
+        position: 'relative',
+        padding: '0.5em',
+    },
+    ellipsis: {
+        position: 'absolute',
+        right: '0.4em',
+        border: '1px dashed grey',
+        borderRadius: '0.5em',
+        padding: '0 0.5em',
+        '&:hover': {
+            background: 'red',
+        },
+    },
+    img: {
+        width: '90%',
+        height: '90%',
+        position: 'absolute',
+        top: 0,
+        left: 0,
+    },
+})
 export const SortableItem = (props: any) => {
     const {
         attributes, listeners, setNodeRef, transform, transition,
@@ -21,31 +46,17 @@ export const SortableItem = (props: any) => {
         [webcamRef]
     )
 
-    const card = {
+    const dynamicCardStyle = {
         transform: CSS.Transform.toString(transform as Transform),
         transition,
-        border: '1px orange solid',
-        position: 'relative',
         ...(props.style ? props.style : {}),
-        padding: '0.5em',
     }
 
-    const ellipsis = {
-        position: 'absolute',
-        right: '0.4em',
-    }
-
-    const img = {
-        width: '90%',
-        height: '90%',
-        position: 'absolute',
-        top: 0,
-        left: 0,
-    }
+    const classes = useStyles()
 
     if (props.data.id === 'camera') {
         return (
-            <div style={card}>
+            <div style={dynamicCardStyle} className={classes.card}>
                 CAMERA
                 {/* <Webcam
                     audio={false}
@@ -61,12 +72,12 @@ export const SortableItem = (props: any) => {
     }
 
     return (
-        <div style={card}>
-            <span style={ellipsis} ref={setNodeRef} {...attributes} {...listeners}>
+        <div style={dynamicCardStyle} className={classes.card}>
+            <span className={classes.ellipsis} ref={setNodeRef} {...attributes} {...listeners}>
                 <FontAwesomeIcon icon={icon} />
             </span>
             <h3>{props.data.id}</h3>
-            {props.data.img && <img style={img} src={props.data.img} />}
+            {props.data.img && <img className={classes.img} src={props.data.img} />}
         </div>
     )
 }
