db44acedb90b88da4e523528702671116c7eb9a8
andrew-oxenburgh <andrew.oxenburgh@gmail.com>
2024-03-19 18:40:41 +1300

    added spinner while camera loads

--- src/pages/be-spectacled/BeSpectacled/Camera.tsx
+++ src/pages/be-spectacled/BeSpectacled/Camera.tsx
@@ -23,6 +23,13 @@ const useStyles = createUseStyles({
         bottom: '10px',
         left: 'calc(25% - 5px)',
     },
+    spinner: {
+        position: 'absolute',
+        left: 'calc(50% - 2.5em)',
+        top: 'calc(50% - 2.5em)',
+        width: '5em',
+        height: '5em',
+    },
 })
 export const Camera = ({ saveImage, onCancel }: {
     saveImage: (img: string) => void
@@ -38,17 +45,19 @@ export const Camera = ({ saveImage, onCancel }: {
             // console.log(imageSrc)
             saveImage(imageSrc)
         } catch (e) {
+            // eslint-disable-next-line no-console
             console.error('error', e)
         }
     }
 
     return (
-        <div className={classes.container}>
+        <div className={classes.container + ' align-content-center'}>
+            {!inited && <div className={classes.spinner + ' spinner-border'}></div>}
             <Webcam
                 audio={false}
                 ref={webcamRef}
                 screenshotFormat="image/jpeg"
-                onUserMedia={() => setInited(true)} />
+                onUserMedia={() => { setInited(true) }} />
             {inited && <Button className={classes.captureButton} onClick={capture}>click</Button>}
             <Button className={classes.cancelButton} onClick={onCancel}>cancel</Button>
         </div>
