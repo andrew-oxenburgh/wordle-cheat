67f58b6135fb9f98528585d146c3caf73102c04e
andrew-oxenburgh <andrew.oxenburgh@gmail.com>
2024-03-19 17:54:17 +1300

    pulled Album into separate component

---
+++ src/pages/be-spectacled/BeSpectacled/Album.tsx
@@ -0,0 +1,85 @@
+import { useState } from 'react';
+import * as R from 'ramda';
+import useMobileDetect from '@groupher/use-mobile-detect-hook';
+import {
+    DndContext,
+    TouchSensor,
+    PointerSensor,
+    useSensor,
+    useSensors,
+    DragEndEvent
+} from '@dnd-kit/core';
+import {
+    arrayMove,
+    SortableContext
+} from '@dnd-kit/sortable';
+import { GotoPhotoBooth } from './GotoPhotoBooth';
+import { Bin } from './Bin';
+import { deleteCollisionDetection } from './deleteCollisionDetection';
+import { DELETEABLE, type Item } from './utils';
+import { PhotoFrames } from './PhotoFrames';
+import { useStyles, findItemIndexById } from './BeSpectacled';
+
+export const Album = ({ items, setItems, openPhotoBooth, deleteMe }: {
+    items: Item[];
+    setItems: (items: Item[] | ((items: Item[]) => Item[])) => void;
+    openPhotoBooth: () => void;
+    deleteMe: (id: number) => void;
+}) => {
+    const [dragging, setDragging] = useState(false);
+    const detectDeviceType = useMobileDetect();
+
+    const sensors = useSensors(
+        useSensor(detectDeviceType.isMobile ? TouchSensor : PointerSensor)
+    );
+    const classes = useStyles();
+    const itemStyle = {
+        minWidth: '33.33%',
+        maxWidth: '33.33%',
+        height: '50%',
+    };
+
+
+    const handleDragEnd = (event: DragEndEvent) => {
+        const { active, over } = event;
+
+        if (over?.id === DELETEABLE) {
+            setDragging(true);
+            const newItems: Item[] = R.filter((i: Item) => { return i.id !== active.id; }, items);
+            setItems(newItems);
+        } else if (active.id !== over?.id) {
+            setItems((i: Item[]) => {
+                const oldIndex = findItemIndexById(active.id, i);
+                const newIndex = findItemIndexById((over?.id || -1), i);
+                return arrayMove(i, oldIndex, newIndex);
+            });
+        }
+        setDragging(false);
+    };
+    return (<DndContext
+        // measuring={measuringConfig}
+        sensors={sensors}
+        collisionDetection={deleteCollisionDetection}
+        onDragEnd={handleDragEnd}
+        onDragStart={() => { setDragging(true); }}
+    >
+        <div style={{ position: 'relative' }}>
+            <SortableContext
+                items={items}
+            >
+                <div className={classes.container}>
+                    <PhotoFrames
+                        items={items}
+                        itemStyle={itemStyle}
+                        deleteMe={deleteMe} />
+                    <GotoPhotoBooth
+                        show={items.length < 6}
+                        style={itemStyle}
+                        key={'camera'}
+                        openPhotoBooth={openPhotoBooth} />
+                </div>
+            </SortableContext>
+            <Bin show={dragging} />
+        </div>
+    </DndContext>);
+};
--- src/pages/be-spectacled/BeSpectacled/BeSpectacled.tsx
+++ src/pages/be-spectacled/BeSpectacled/BeSpectacled.tsx
@@ -3,29 +3,16 @@ import * as R from 'ramda'
 import useMobileDetect from '@groupher/use-mobile-detect-hook'
 
 import {
-    DndContext,
-    TouchSensor,
-    PointerSensor,
-    useSensor,
-    useSensors,
-    DragEndEvent,
     UniqueIdentifier,
 } from '@dnd-kit/core'
-import {
-    arrayMove,
-    SortableContext,
-} from '@dnd-kit/sortable'
 
-import { GotoPhotoBooth } from './GotoPhotoBooth'
-import { Bin } from './Bin'
 import { createUseStyles } from 'react-jss'
-import { deleteCollisionDetection } from './deleteCollisionDetection'
-import { DELETEABLE, Item, _items } from './utils'
+import { type Item, _items } from './utils'
 import { EtAl } from './EtAl'
-import { PhotoFrames } from './PhotoFrames'
 import { Camera } from './Camera'
+import { Album } from './Album'
 
-const useStyles = createUseStyles({
+export const useStyles = createUseStyles({
     container: {
         width: '100%',
         maxWidth: '60em',
@@ -37,44 +24,15 @@ const useStyles = createUseStyles({
     },
 })
 
+export const findItemIndexById = (id: UniqueIdentifier, items: Item[]): number => {
+    return R.findIndex((v: Item) => (v.id === id), items) as number
+}
+
 export const BeSpectacled = () => {
     const [items, setItems] = useState(_items)
-    const [photoBooth, setPhotoBooth] = useState(false)
-    const [dragging, setDragging] = useState(false)
     const [cnt, setCnt] = useState(100)
-
+    const [photoBooth, setPhotoBooth] = useState(false)
     const detectDeviceType = useMobileDetect()
-
-    const sensors = useSensors(
-        useSensor(detectDeviceType.isMobile ? TouchSensor : PointerSensor)
-    )
-
-    const findItemIndexById = (id: UniqueIdentifier): number => {
-        return R.findIndex((v: Item) => (v.id === id), items) as number
-    }
-
-    const handleDragEnd = (event: DragEndEvent) => {
-        const { active, over } = event
-
-        if (over?.id === DELETEABLE) {
-            setDragging(true)
-            const newItems: Item[] = R.filter((i: Item) => { return i.id !== active.id }, items)
-            setItems(newItems)
-        } else if (active.id !== over?.id) {
-            setItems((i: Item[]) => {
-                const oldIndex = findItemIndexById(active.id)
-                const newIndex = findItemIndexById((over?.id || -1))
-                return arrayMove(i, oldIndex, newIndex)
-            })
-        }
-        setDragging(false)
-    }
-
-    const itemStyle = {
-        minWidth: '33.33%',
-        maxWidth: '33.33%',
-        height: '50%',
-    }
 
     const appendImage = (item: Item) => {
         const newItems: Item[] = R.concat(items, [item])
@@ -106,39 +64,20 @@ export const BeSpectacled = () => {
         setPhotoBooth(false)
     }
 
-    const classes = useStyles()
-    const Album = () => {
-        return (<DndContext
-            // measuring={measuringConfig}
-            sensors={sensors}
-            collisionDetection={deleteCollisionDetection}
-            onDragEnd={handleDragEnd}
-            onDragStart={() => { setDragging(true) }}
-        >
-            <div style={{ position: 'relative' }}>
-                <SortableContext
-                    items={items}
-                >
-                    <div className={classes.container}>
-                        <PhotoFrames
-                            items={items}
-                            itemStyle={itemStyle}
-                            deleteMe={deleteMe} />
-                        <GotoPhotoBooth
-                            show={items.length < 6}
-                            style={itemStyle}
-                            key={'camera'}
-                            openPhotoBooth={openPhotoBooth} />
-                    </div>
-                </SortableContext>
-                <Bin show={dragging} />
-            </div>
-        </DndContext>)
-    }
-
     return (
         <>
-            {photoBooth ? <Camera onCancel={cancel} saveImage={saveImage} /> : <Album />}
+            {photoBooth ?
+                <Camera
+                    onCancel={cancel}
+                    saveImage={saveImage}
+                /> :
+                <Album
+                    items={items}
+                    setItems={setItems}
+                    deleteMe={deleteMe}
+                    openPhotoBooth={openPhotoBooth}
+                />
+            }
             <EtAl
                 items={items}
                 setItems={setItems}
