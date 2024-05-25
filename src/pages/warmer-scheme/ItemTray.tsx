b1d74605e3818f0b28d2f719fa86ba8f2c683349
andrew-oxenburgh <andrew.oxenburgh@gmail.com>
2024-05-25 15:29:46 +1200

    chore: Update @preact/signals-react to version 2.0.1 and adjust ItemTray styling and functionality

--- src/pages/warmer-scheme/ItemTray.tsx
+++ src/pages/warmer-scheme/ItemTray.tsx
@@ -1,7 +1,14 @@
 import Card from 'react-bootstrap/Card'
+import ButtonGroup from 'react-bootstrap/ButtonGroup'
+import Button from 'react-bootstrap/Button'
 import { ItemTrayProps } from './item-props'
+import { useSignal, useSignalEffect, useSignals } from '@preact/signals-react/runtime'
 
 export const ItemTray: React.FC<ItemTrayProps> = ({ item }) => {
+    const counter = useSignal(0)
+    useSignals()
+    useSignalEffect(() => { })
+
     if (!item) {
         return (
             <>
@@ -12,13 +19,25 @@ export const ItemTray: React.FC<ItemTrayProps> = ({ item }) => {
     const id = item.id
     const name = item.name
     const desc = item.desc
-    const h5em = { height: '5em' }
+    const h5em = { height: '10em' }
+    const w3em = {
+        display: 'inline-block',
+        width: '3em',
+    }
     const sq5em = {
         width: '5em',
         height: '5em',
         position: 'absolute' as any,
         top: '0.2em',
         right: '0.5em',
+    }
+    const add = () => {
+        counter.value++
+    }
+    const subtract = () => {
+        if (counter.value > 0) {
+            counter.value--
+        }
     }
     return (
         <Card className="p-1 m-1" >
@@ -27,6 +46,20 @@ export const ItemTray: React.FC<ItemTrayProps> = ({ item }) => {
                     <div className='w-75'>
                         <h5 className="w-100">{name}</h5>
                         <i>{desc}</i>
+                        <br />
+                        <span className="w-100">
+                            <span style={w3em}>{counter.value}</span>
+                            <ButtonGroup aria-label="Basic example" className="w-50">
+                                <Button
+                                    variant="light"
+                                    onClick={add}
+                                >+</Button>
+                                <Button
+                                    onClick={subtract}
+                                    variant="light"
+                                >-</Button>
+                            </ButtonGroup>
+                        </span>
                     </div>
                     <Card.Img
                         src={`/warmer-scheme/img/${id}.png`}
