b43107aa6d668c9e9ffacf91d455ade3dad2c759
andrew-oxenburgh <andrew.oxenburgh@gmail.com>
2024-06-22 15:47:50 +1200

    added picklist

--- src/pages/warmer-scheme/WarmerSchema.tsx
+++ src/pages/warmer-scheme/WarmerSchema.tsx
@@ -12,6 +12,30 @@ import Button from 'react-bootstrap/Button'
 import * as R from 'ramda'
 import { useState } from 'react'
 import { BakeItems } from './item-props'
+import Table from 'react-bootstrap/Table'
+
+const PickList = ({ items }: { items: BakeItems[] }) => {
+    return (
+        <Table striped bordered hover>
+            <thead>
+                <tr>
+                    <th>#</th>
+                    <th>Name</th>
+                    <th>count</th>
+                </tr>
+            </thead>
+            <tbody>
+                {R.keys(items).map((item, index) => (
+                    <tr key={item}>
+                        <td>{index + 1}</td>
+                        <td>{item}</td>
+                        <td>{items[item]}</td>
+                    </tr>
+                ))}
+            </tbody>
+        </Table>
+    )
+}
 
 const BakesDropdown = ({ selectedBake }: { selectedBake: Signal<string> }) => {
     useSignals()
@@ -36,13 +60,14 @@ const WarmerScheme: React.FC = () => {
     const selectedBake = useSignal(listOfBakes()[0])
     useSignals()
     useSignalEffect(() => { })
+    const [pickList, setShowPickList] = useState<boolean>(false)
 
     const [itemCounts, setItemCounts] = useState<BakeItems[]>([])
 
     const setItemCount = (itemId: string) => {
         return (inc: number) => {
-            const newCounts = {...itemCounts}
-            if (!newCounts[itemId]){
+            const newCounts: BakeItems[] = { ...itemCounts }
+            if (!newCounts[itemId]) {
                 newCounts[itemId] = 0
             }
             newCounts[itemId] = newCounts[itemId] + inc
@@ -51,36 +76,39 @@ const WarmerScheme: React.FC = () => {
         }
     }
 
-    const pickList = () => {
-        // eslint-disable-next-line no-console
-        console.log('pick list', itemCounts)
-
-    }
-
     const warmerConfig = R.splitEvery(warmerWidth, itemPositions)
     return (
         <PageBody name="warmer-scheme">
             <Container className='m-0'>
-                <BakesDropdown selectedBake={selectedBake} />
-                {warmerConfig.map((row: string[], rowIndex: number) => (
-                    <Row key={rowIndex}>
-                        {row.map((item: string, colIndex) => (
-                            <Col key={colIndex} className="p-1">
-                                {
-                                    <ItemTray
-                                        itemId={item}
-                                        selectedBake={selectedBake.value}
-                                        setItemCount={setItemCount(item)}
-                                        counter={itemCounts[item] || 0 }
-                                    />
-                                }
-                            </Col>
-                        ))}
-                    </Row>
-                ))}
                 <Button onClick={() => {
-                    pickList()
-                }}>create pick list</Button>
+                    setShowPickList(!pickList)
+                }}>toggle pick list</Button>
+
+                {
+                    pickList ? (<PickList items={itemCounts}/>) :
+                        (
+                            <>
+                                <BakesDropdown selectedBake={selectedBake} />
+                                {warmerConfig.map((row: string[], rowIndex: number) => (
+                                    <Row key={rowIndex}>
+                                        {row.map((item: string, colIndex) => (
+                                            <Col key={colIndex} className="p-1">
+                                                {
+                                                    <ItemTray
+                                                        itemId={item}
+                                                        selectedBake={selectedBake.value}
+                                                        setItemCount={setItemCount(item)}
+                                                        counter={itemCounts[item] || 0}
+                                                    />
+                                                }
+                                            </Col>
+                                        ))}
+                                    </Row>
+                                ))}
+                            </>
+                        )
+                }
+
             </Container>
         </PageBody >
     )
